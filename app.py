ccd681ab4cb840461d5cdc8197242af16e0c12d0
Dan Getelman <dgetelman@gmail.com>
2014-04-28 13:40:34 -0700

    Add all of our routes

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask
 app = Flask(__name__)
+app.debug = True
 
 @app.route("/")
-def hello():
-    return "Hello world!"
+def home():
+    return "Skill Camp!"
+
+@app.route("/create")
+def create():
+    return "Make a new thing!"
+
+@app.route("/<int:uid>/view")
+def view(uid):
+    return "Look at %d" % (uid,)
+
+@app.route("/<int:uid>/edit")
+def edit(uid):
+    return "I'm editing %d" % (uid,)
 
 if __name__ == "__main__":
     app.run()

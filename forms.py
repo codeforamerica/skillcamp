68cdd76768de8a668cc08f325abcc0a2dbd24b59
Maya Benari <maya@codeforamerica.org>
2014-06-04 20:49:59 -0700

    Add text area fields to create lesson page

--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
 # Our main form
 from flask_wtf import Form
-from wtforms import TextField, FieldList, FormField
+from wtforms import TextField, TextAreaField, FieldList, FormField
 from wtforms.validators import DataRequired
 
 class StepField(Form):
     title = TextField('Title', validators=[])
-    body = TextField('Body', validators=[])
+    body = TextAreaField('Body', validators=[])
 
 class AssetField(Form):
     title = TextField('Title', validators=[])
@@ -19,12 +19,12 @@ class LessonForm(Form):
     title = TextField('Title', validators=[])
     time = TextField('Time', validators=[])
     audience = TextField('Audience', validators=[])
-    goals = TextField('Goals', validators=[])
-    summary = TextField('Summary', validators=[])
+    goals = TextAreaField('Goals', validators=[])
+    summary = TextAreaField('Summary', validators=[])
     steps = FieldList(FormField(StepField), min_entries=5)
     assets = FieldList(FormField(AssetField), min_entries=1)
     resources = FieldList(FormField(ResourceField), min_entries=1)
 
     author_name = TextField('Author Name', validators=[])
     author_image = TextField('Author Image URL', validators=[])
-    author_bio = TextField('Author Bio', validators=[])
+    author_bio = TextAreaField('Author Bio', validators=[])
